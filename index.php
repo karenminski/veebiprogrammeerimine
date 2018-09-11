aa68faf086dd10b8638d646c4958c045cf278d59
Karen Minski <karenminski@users.noreply.github.com>
2018-09-11 14:22:20 +0300

    lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>Karen Minski, õppetöö</title>
+  </head>
+  <body>
+  <h1>Karen Minski</h1>
+  <p>Siin on minu <a href="http://www.tlu.ee">TLÜ</a> õppetöö raames valminud leht ja see ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+  <p>Lisatud tekst on tehtud kodusetöö raames ning on mõeldud vaid testimiseks.</p>
+  <img src="http://cdn.akc.org/content/hero/puppy-boundaries_header.jpg" alt="kutsikas">
+  <p>Minu sõber teeb ka <a href="../../~kellrei" target="_blank">veebi</a></p>
+  </body>
+  
+</html>

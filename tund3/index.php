3463a30d791c5c522e988f6cef59ee14d1fbfb59
Karen Minski <karenminski@users.noreply.github.com>
2018-09-18 15:33:33 +0300

    lisatud massiiv, tsükkel ja kataloogi lugemine

---
+++ tund3/index.php
@@ -0,0 +1,66 @@
+<?php
+ //echo "See on minu esimene php!";
+ $firstName = "Karen";
+ $lastName = "Minski";
+ $dateToday = date("d/m/y");
+ $weekdayToday = date("N");
+ $weekdayNamesET = ["esmaspäev","teisipäev","kolmapäev","neljapäev","reede","laupäev","pühapäev"];
+ //echo $weekdayNamesET;
+ //var_dump($weekdayNamesET);
+ //echo $weekdayNamesET[1];
+ //echo $weekdayToday;
+ $hourNow = date("G");
+ $partOfDay = "";
+ if ($hourNow < 8) {
+	 $partOfDay = "varajane hommik";
+ }
+  if ($hourNow >= 8 and $hourNow < 16) {
+	 $partOfDay = "koolipäev";
+ }
+  if ($hourNow > 16) {
+	 $partOfDay = "loodetavasti vaba aeg";
+	 
+	} 
+	 //juhusliku pildi valimine
+	 $picURL = " http://www.cs.tlu.ee/~rinde/media/fotod/TLU_600x400/tlu_";
+	 $picEXT = ".jpg";
+	 $picNUM = mt_rand(2,43);
+	 //echo $picNUM;
+	 $picFILE = $picURL .$picNUM .$picEXT;
+ 
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+  echo $firstName;
+  echo " ";
+  echo $lastName;
+  ?>
+  , õppetöö</title>
+  </head>
+  <body>
+  <h1>
+  <?php
+  echo $firstName ." " .$lastName; 
+  ?>
+  </h1>
+  <p>Siin on minu <a href="http://www.tlu.ee">TLÜ</a> õppetöö raames valminud leht ja see ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+  <p>Lisatud tekst on tehtud kodusetöö raames ning on mõeldud vaid testimiseks.</p>
+  <p><a href="page.php">Page.php</a>, <a href="photo.php">photo.php</a></p>
+  
+  <?php
+  // echo "<p> Tänane kuupäev on: " .$dateToday .".</p> \n";
+  echo "<p>Täna on " .$weekdayNamesET[$weekdayToday - 1] .", " .$dateToday .". </p> \n";
+  echo "<p> Lehe avamise hetkel oli kell " .date("H:i:s") .", käes oli " .$partOfDay .".</p> \n";
+  ?>
+  <img src="<?php echo $picFILE; ?>" alt="TLÜ">
+  <img src="http://cdn.akc.org/content/hero/puppy-boundaries_header.jpg" alt="kutsikas">
+  <p>Minu sõber teeb ka <a href="../../../~kellrei/veebiprogrammeerimine/Tund3" target="_blank">veebi</a></p>
+  </body>
+  
+</html>
---
+++ tund3/page.php
@@ -0,0 +1,61 @@
+<?php
+ //echo "See on minu esimene php!";
+ $firstName = "Tundmatu";
+ $lastName = "Kodanik";
+//Püüan POST andmed kinni
+ //var_dump($_POST);
+ if (isset($_POST["firstname"])){
+ $firstName = $_POST["firstname"];
+ }
+  if (isset($_POST["lastname"])){
+ $lastName = $_POST["lastname"];
+ }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+  echo $firstName;
+  echo " ";
+  echo $lastName;
+  ?>
+  , õppetöö</title>
+  </head>
+  <body>
+  <h1>
+  <?php
+  echo $firstName ." " .$lastName; 
+  ?>
+  </h1>
+  <p>Siin on minu <a href="http://www.tlu.ee">TLÜ</a> õppetöö raames valminud leht ja see ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+  <p>Lisatud tekst on tehtud kodusetöö raames ning on mõeldud vaid testimiseks. </p>
+  <hr>
+  
+<form method ="POST">
+  <label> Eesnimi :</label>
+  <input type ="text" name="firstname">
+  <label> Perekonnanimi :</label>
+  <input type ="text" name="lastname">
+  <label>Sünniaasta:</label>
+  <input type="number" min="1914" max="2000" value="1999" name="birthyear">
+  <input type="submit" name="submitUserData" value="Saada andmed">
+</form>
+<hr>
+<?php
+if (isset($_POST["birthyear"])) {
+	echo "<p>Olete elanud järgnevatel aastatel:</p> \n";
+	echo "<ul> \n";
+	  for ($i = $_POST["birthyear"]; $i <= date("Y"); $i++) {
+		  echo"<li>" .$i ."</li> \n";
+	  }
+	echo "</ul> \n";	
+}
+
+?>
+  
+  </body>
+  
+</html>

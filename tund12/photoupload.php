be53c06712d1c2e81f450c0dca3e9786c3f59a82
Karen Minski <karenminski@users.noreply.github.com>
2018-11-27 15:17:17 +0200

    Tund12

---
+++ tund12/photoupload.php
@@ -0,0 +1,122 @@
+<?php
+  require("functions.php");
+  require("classes/Photoupload.class.php");
+  require("../../../config.php");
+  // kui pole sisse loginud
+  if(!isset($_SESSION["userId"])){
+	  header("Location: index2.php");
+	  exit();
+  }
+  //Välja logimine
+  if(isset ($_GET["logout"])){
+	  session_destroy(); 
+	  header("Location: index2.php");
+	  exit();
+  }
+  
+  /* require("classes/Test.class.php");
+  $myNumber = new Test(7);
+  echo "Avalik arv on: " .$myNumber->publicNumber;
+  //echo "Salajane arv on: " .$myNumber->secretNumber;
+  $myNumber->tellThings();
+  $mySNumber = new Test(9);
+  echo " Teine avalik arv on: " .$mySNumber->publicNumber;
+  unset($myNumber); */
+  
+  //Piltide üleslaadimise osa
+    $notice = "";
+	//$target_dir = "../vppicuploads/";
+	$target_dir = $picDir;
+	//$thumb_dir = "../vp_thumb_uploads/";
+	$thumb_dir = $thumbDir;
+	$thumbSize = 100;
+	$targetFile = "";
+	$uploadOk = 1;
+	$imageNamePrefix = "vp_";
+    $textToImage = "Veebiprogrammeerimine";
+    $pathToWatermark = "../vp_picfiles/vp_logo_w100_overlay.png";
+	
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submitImage"])) {
+		//var_dump($_POST);
+		//var_dump($_FILES);
+		//kas failinimi ka olemas on
+		if(!empty($_FILES["fileToUpload"]["name"])){
+		
+			$myPhoto = new Photoupload($_FILES["fileToUpload"]);
+			$myPhoto->readExif();
+			//echo $myPhoto->photoDate;
+			if(!empty($myPhoto->photoDate)){
+				$textToImage = $myPhoto->photoDate;
+			} else {
+				$textToImage = "Pildistamise aeg teadmata";
+			}
+			$myPhoto->makeFileName($imageNamePrefix);
+			//määrame faili nime
+			$target_file = $target_dir .$myPhoto->fileName;
+			
+			//kas on pilt
+			$uploadOk = $myPhoto->checkForImage();
+			if($uploadOk == 1){
+			  // kas on sobiv tüüp
+			  $uploadOk = $myPhoto->checkForFileType();
+			}
+			
+			if($uploadOk == 1){
+			  // kas on sobiv suurus
+			  $uploadOk = $myPhoto->checkForFileSize($_FILES["fileToUpload"], 2500000);
+			}
+			
+			if($uploadOk == 1){
+			  // kas on juba olemas
+			  $uploadOk = $myPhoto->checkIfExists($target_file);
+			}
+						
+			// kui on tekkinud viga
+			if ($uploadOk == 0) {
+				$notice = "Vabandame, faili ei laetud üles! Tekkisid vead: ".$myPhoto->errorsForUpload;
+			// kui kõik korras, laeme üles
+			} else {
+				
+				$myPhoto->resizeImage(600, 400);
+				$myPhoto->addWatermark($pathToWatermark);
+				$myPhoto->addText($textToImage, 20, 25);
+				$saveResult = $myPhoto->savePhoto($target_file);
+				//kui salvestus õnnestus, lisame andmebaasi
+				if($saveResult == 1){
+				  $myPhoto->createThumbnail($thumb_dir, $thumbSize);
+                  $notice = "Foto laeti üles! ";
+				  $notice .= addPhotoData($myPhoto->fileName, $_POST["altText"], $_POST["privacy"]);
+				} else {
+                  $notice .= "Foto lisamisel andmebaasi tekkis viga!";
+                }
+				
+			}
+			unset($myPhoto);
+		}//ega failinimi tühi pole
+	}
+	// Lehe päise laadimine
+	$pageTitle = "Piltide üleslaadimine";
+	$scripts = '<script type="text/javascript" src="javascript/checkFileSize.js" defer></script>' ."\n";
+	require("header.php");
+?>
+
+<!DOCTYPE html>
+<html>
+  <body>
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+    <label>Vali üleslaetav pilt:</label>
+    <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+	<label>Pildi kirjeldus (max 256 tähemärki): </label>
+	<input type="text" name="altText">
+	<br><br>
+	<label>Pildi kasutusõigused</label><br>
+	<input type="radio" name="privacy" value="1"><label>Avalik</Label><br>
+	<input type="radio" name="privacy" value="2"><label>Sisseloginud kasutajatele</Label><br>
+	<input type="radio" name="privacy" value="3" checked><label>Privaatne</Label><br><br>
+    <input id="submitImage" type="submit" value="Lae pilt üles" name="submitImage"><span id="infoPlace"><?php echo $notice; ?></span>
+</form>
+<br>
+<?php require("footer.php"); ?>
+  </body>
+</html>
---
+++ tund12/pubgallery.php
@@ -0,0 +1,73 @@
+<?php
+  require("functions.php");
+  //kui pole sisse loginud
+  if(!isset($_SESSION["userId"])){
+	  header("Location: index2.php");
+	  exit();
+  }
+  
+  //väljalogimine
+  if(isset($_GET["logout"])){
+	session_destroy();
+	header("Location: index2.php");
+	exit();
+  }
+  $page = 1;
+  $totalImages = findTotalPublicImages();
+  //echo $totalImages;
+  $limit = 10;
+  if(!isset($_GET["page"]) or $_GET["page"] < 1){
+	  $page = 1;
+  } elseif (round(($_GET["page"] - 1) * $limit) > $totalImages){
+	  $page = round($totalImages / $limit);
+  } else {
+	  $page = $_GET["page"];
+  }
+  //$publicThumbnails = readAllPublicPictureThumbs();
+  $publicThumbnails = readAllPublicPictureThumbsPage($page, $limit);
+  
+  //$userslist = listusers();
+  //$thumbs = allPublicPictureThumbsPage(2);
+  $pageTitle = "Avalikud pildid";
+  $scripts = '<link rel="stylesheet" type="text/css" href="style/modal.css">';
+  $scripts .= '<script type="text/javascript" src="javascript/modal.js" defer></script>' ."\n";
+  require("header.php");
+?>
+
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>
+	  <?php
+	    echo $_SESSION["userFirstName"] ." " .$_SESSION["userLastName"];
+	  ?>
+	</h1>
+	<!-- The Modal -->
+	<div id="myModal" class="modal">
+		<!-- The Close Button -->
+		<span class="close">&times;</span>
+		<!-- Modal Content (The Image) -->
+		<img class="modal-content" id="modalImg">
+		<!-- Modal Caption (Image Text) -->
+		<div id="caption"></div>
+	</div>
+	<div id="gallery">
+	<?php
+		echo "<p>";
+		if ($page > 1){
+			echo '<a href="?page=' .($page - 1) .'">Eelmised pildid</a> ';
+		} else {
+			echo "<span>Eelmised pildid</span> ";
+		}
+		if ($page * $limit < $totalImages){
+			echo '| <a href="?page=' .($page + 1) .'">Järgmised pildid</a>';
+		} else {
+			echo "| <span>Järgmised pildid</span>";
+		}
+		echo "</p> \n";
+		echo $publicThumbnails;
+	?>
+	</div>
+	<?php require("footer.php"); ?>
+  </body>
+</html>
